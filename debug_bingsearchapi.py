3ca68fcca6d1c647fc2771b964eb1377212265e7
Frank Xu <frankxu2004@gmail.com>
2023-02-13 00:53:38 -0500

    update header to make bing search api determinisitc

--- debug_bingsearchapi.py
+++ debug_bingsearchapi.py
@@ -12,29 +12,37 @@ Documentation: https://docs.microsoft.com/en-us/bing/search-apis/bing-web-search
 # subscription_key = os.environ['BING_SEARCH_V7_SUBSCRIPTION_KEY']
 # endpoint = os.environ['BING_SEARCH_V7_ENDPOINT'] + "/bing/v7.0/search"
 
-def bing_search(queries):
-    subscription_key = "YOUR_OWN_API_KEY"
+def bing_search(query):
+    # subscription_key = "YOUR_OWN_API_KEY"
+    subscription_key = ""
     endpoint = "https://api.bing.microsoft.com/" + "v7.0/search"
-
-    for query in queries:
-        # Query term(s) to search for. 
-        query = "hydrogen atomic number"
-
-        # Construct a request
-        mkt = 'en-US'
-        params = { 'q': query, 'mkt': mkt, 'responseFilter':['Webpages'], "count": 20}
-        headers = { 'Ocp-Apim-Subscription-Key': subscription_key }
-
-        # Call the API
-        try:
-            response = requests.get(endpoint, headers=headers, params=params)
-            response.raise_for_status()
-
-            # print("Headers:")
-            # print(response.headers)
-
-            print("JSON Response:")
-            pprint(response.json())
-        except Exception as ex:
-            raise ex
     
+    # Construct a request
+    params = {'q': query, 
+              'mkt': 'en-US', 
+              'responseFilter':['Webpages'], 
+              "count": 5,
+              "safeSearch": 'Off',
+              "setLang": 'en-US'}
+    
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key ,
+               'Pragma': 'no-cache',
+               'X-MSEdge-ClientID':'05EFBDF2399564422335AF4538A8655D',
+               'User-Agent': "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko",
+               'X-MSEdge-ClientIP': "128.2.211.82"}
+
+    # Call the API
+    try:
+        response = requests.get(endpoint, headers=headers, params=params)
+        response.raise_for_status()
+
+        print("Headers:")
+        print(response.headers)
+
+        print("JSON Response:")
+        pprint(response.json())
+    except Exception as ex:
+        raise ex
+
+if __name__ == "__main__":
+    bing_search("Can you transport a primate in a backpack?")
